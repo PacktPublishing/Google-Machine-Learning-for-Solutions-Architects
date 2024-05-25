fd45f5a1f9d1d2f1e311a6f9fb4740b3605a48d6
Kieran Kavanagh <46874938+kierankav@users.noreply.github.com>
2024-05-25 13:36:27 -0400

    Update chapter-18-prereqs.sh
    Added Document AI API User role

--- Chapter-18/prereqs/chapter-18-prereqs.sh
+++ Chapter-18/prereqs/chapter-18-prereqs.sh
@@ -6,5 +6,8 @@ export PROJECT_NUMBER=`gcloud projects describe $GOOGLE_CLOUD_PROJECT --format='
 # Grant the "Eventarc Event Receiver" IAM role to your project's default Compute Engine service account:
 gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member=serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com --role=roles/eventarc.eventReceiver
 
+# Grant the "Document AI API User" IAM role to your project's default Compute Engine service account:
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member=serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com --role="roles/documentai.apiUser"
+
 # Grant the "Service Account Token Creator" IAM role to the Pub/Sub service agent for your project:
 gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-pubsub.iam.gserviceaccount.com --role=roles/iam.serviceAccountTokenCreator
